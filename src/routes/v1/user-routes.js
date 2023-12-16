85ef9000614bd960975f56d3e2c8d83faa2759cf
Md Areeb Ansar <mdareebansar@gmail.com>
2023-12-16 23:09:48 +0530

    Added middlewares for signin api

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { UserController } = require("../../controllers");
+const { AuthRequestMiddleware } = require("../../middlewares");
 const router = express.Router();
 
 router.post("/signup", UserController.signup);
-router.post("/signin", UserController.signin);
+router.post(
+  "/signin",
+  AuthRequestMiddleware.validateSigninRequest,
+  UserController.signin
+);
 
 module.exports = router;
