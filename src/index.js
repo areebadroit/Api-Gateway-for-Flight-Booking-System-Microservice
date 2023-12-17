d74d04781617772222803de2708d4d479e56aa6f
Md Areeb Ansar <mdareebansar@gmail.com>
2023-12-17 14:18:57 +0530

    Set up reverse proxy using 'http-proxy-middleware' to forward request to other services from the gateway

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 const express = require("express");
 
 const { ServerConfig, Logger } = require("./config");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const apiRoutes = require("./routes");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 30, // Limit each IP to 30 requests per `window` (here, per 2 minutes)
+});
+
 app.use(express.json()); //Helps to parse the incoming request body as json
 app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
 
 app.use("/api", apiRoutes);
+app.use(
+  "/flightsservice",
+  createProxyMiddleware({
+    target: "http://127.0.0.1:3000/",
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/bookingservice",
+  createProxyMiddleware({
+    target: "http://127.0.0.1:4000/",
+    changeOrigin: true,
+  })
+);
 app.listen(ServerConfig.PORT, async () => {
   console.log(`Server started at PORT: ${ServerConfig.PORT}`);
   Logger.info("Server up and running.");
